6aaccb3fc82ad86fb880a1253e4e7a826c772e8c
Jacob Davis <davisjk972@gmail.com>
2019-05-04 22:52:19 -0600

    Generate markup file and download

--- converter.js
+++ converter.js
@@ -1,11 +1,23 @@
+// define String.format if it isn't implemented yet
+if (!String.prototype.format) {
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
+        });
+    };
+}
+
 var showing = "None";
+var markup = "";
+var tsv = "";
 
 $(document).ready(function () {
     $("#tsv-markup #file").change(function () {
-        var tsv = $("#tsv-markup #file")[0].files[0];
-        if (tsv) {
+        var file = $("#tsv-markup #file")[0].files[0];
+        if (file) {
             var reader = new FileReader();
-            reader.readAsText(tsv);
+            reader.readAsText(file);
             reader.onload = function (e) {
                 $("#tsv-markup #input").html(e.target.result);
                 $("#tsv-markup #output").html(generateMarkup(e.target.result));
@@ -20,13 +32,14 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     $("#markup-tsv #file").change(function () {
-        var markup = $("#markup-tsv #file")[0].files[0];
-        if (markup) {
+        var file = $("#markup-tsv #file")[0].files[0];
+        if (file) {
             var reader = new FileReader();
-            reader.readAsText(markup);
+            reader.readAsText(file);
             reader.onload = function (e) {
                 $("#markup-tsv #input").html(e.target.result);
-                $("#markup-tsv #output").html(generateTsv(e.target.result));
+                tsv = generateTsv(e.target.result);
+                $("#markup-tsv #output").html(tsv);
             }
         }
         else {
@@ -45,6 +58,8 @@ function tsvToMarkup() {
     $("#tsv").addClass("underline");
     $("#markup").removeClass("underline");
     $("#markup-tsv").hide();
+    $("#tsv-markup #input").hide();
+    $("#tsv-markup #output").hide();
     $("#tsv-markup").show();
     showing = "tsv-markup";
 }
@@ -58,6 +73,8 @@ function markupToTsv() {
     $("#markup").addClass("underline");
     $("#tsv").removeClass("underline");
     $("#tsv-markup").hide();
+    $("#markup-tsv #input").hide();
+    $("#markup-tsv #output").hide();
     $("#markup-tsv").show();
     showing = "markup-tsv";
 }
@@ -86,19 +103,122 @@ function showOutput() {
 
 function download() {
     if (showing == "tsv-markup") {
-        var contents = $('#tsv-markup #output').text();
+        var data = markup;
+        var filename = "MoveDex.txt";
     }
     else if (showing == "markup-tsv") {
-        var contents = $('#markup-tsv #output').text();
+        var data = tsv;
+        var filename = "MoveDex.tsv";
+    }
+    if (!data) {
+        alert("Nothing to download");
+        return;
+    }
+
+    var file = new Blob([data], { type: "text/plain" });
+    if (window.navigator.msSaveOrOpenBlob) {
+        // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    }
+    else {
+        // Others
+        var a = document.createElement("a");
+        var url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function () {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
     }
 }
 
+var markupFormat = "#### {0}\n___\n- **Type:** {1}\n- **Move Power:** {2}\n- **Move Time:** {3}\n- **PP:** {4}\n- **Duration:** {5}\n- **Range:** {6}\n- **Description:**  \n{7}\n- **Higher Levels:**  \n{8}"
+var column = "```\n```"
+var page = "\\page"
+var pageNumber = "<div class='pageNumber auto'></div>";
+var header = "Name\tType\tMove Power\tMove Time\tPP\tDuration\tRange\tDescription Damage\tDescription\tHigher Levels Damage\tHigher Levels Override";
+var linesPerCol = 63;
+var charsPerLine = 58;
+
 function generateMarkup(tsv) {
-    // TODO
-    console.log("generate markup");
+    markup = [pageNumber];
+    colLen = 0;
+    colNum = 0;
+
+    var rows = tsv.replace("}+MOVE", "} + MOVE").split("\n");
+    rows.forEach(row => {
+        row = row.split("\t");
+        var name = row[0];
+
+        // skip the header row
+        if (name == "Name") {
+            continue;
+        }
+
+        var type = row[1];
+        var power = row[2];
+        var time = row[3];
+        var pp = row[4];
+        var duration = row[5];
+        var range = row[6];
+        var damage = row[7];
+        var description = row[8].replace("{}", damage);
+        var higher = row[9].split(",");
+        var override = row[10];
+        var format = markupFormat;
+
+        // remove Higher Levels from markup if we have no data for it
+        if (!higher == [""] && !override) {
+            format = format.replace(/^.*Higher.*$/, "");
+            higher = "";
+        }
+        else if (override) {
+            higher = override;
+        }
+        else {
+            higher = "The base damage of this attack increases to {0} at level 5, {1} at level 11, and {2} at level 17."
+                .format(higher[0], higher[1], higher[2]);
+        }
+
+        var entry = format.format(name, type, power, time, pp, duration, range, description, higher);
+        // might need to do special stuff for é and ’
+        entry = entry.replace("Pokemon", "Pokémon");
+        var len = 0;
+        var lines = entry.split("\n");
+        lines.forEach(line => {
+            len += Math.ceil(line.len / charsPerLine);
+        });
+        if (len + colLen > linesPerCol) {
+            colLen = len;
+            if (colNum == 0) {
+                colNum = 1;
+                markup.push(column);
+            }
+            else {
+                colNum = 0;
+                markup.push(page);
+                markup.push(pageNumber);
+            }
+        }
+        else {
+            colLen += len;
+        }
+        markup.push(entry);
+    });
+
+    markup = markup.join("\n\n");
+    markup.trim();
+    markup.replace(/\n{3,}/g, "\n\n");
+
+    return markup;
 }
 
 function generateTsv(markup) {
     // TODO
     console.log("generate tsv");
+
+
 }
